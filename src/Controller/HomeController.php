4900b9f3a0bc0ca92ad2230113c4b870d231887a
JennyViannay <jenny.viannay75@gmail.com>
2020-11-09 17:13:29 +0100

    WIP::FILTERS

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -35,10 +35,9 @@ class HomeController extends AbstractController
         ]);
     }
     
-    public function articles(array $article = null)
+    public function articles(array $articles = null)
     {
         $result = [];
-        $cartService = new CartService();
         $filterService = new FilterService();
 
         $brandManager = new BrandManager();
@@ -54,12 +53,11 @@ class HomeController extends AbstractController
         $articles = $articleManager->selectAll();
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (!empty($_POST['add_article'])) {
-                $article = $_POST['add_article'];
-                $cartService->add($article);
-            }
-            if (isset($_POST['search']) || isset($_POST['brand_id']) || isset($_POST['color_id']) || isset($_POST['size_id'])) {
+            if (isset($_POST['brand_id']) || isset($_POST['color_id']) || isset($_POST['size_id'])) {
                 $articles = $filterService->getArticlesFromSearch($_POST);
+            }
+            if (isset($_POST['search']) && !empty($_POST['search'])) {
+                $articles = $articleManager->searchByModel($_POST['search']);
             }
         }
 
@@ -132,6 +130,7 @@ class HomeController extends AbstractController
                     $cartService->payment($_POST);
                 } else {
                     $_SESSION['flash_message'] = ["Tous les champs sont obligatoires !"];
+                    header('Location:/home/cart');
                 }
             }
             if (isset($_POST['update_cart'])) {
@@ -141,7 +140,7 @@ class HomeController extends AbstractController
                 $article = $_POST['add_article'];
                 $cartService->add($article);
             }
-            if (isset($_POST['search']) || isset($_POST['brand_id']) || isset($_POST['color_id']) || isset($_POST['size_id'])) {
+            if (isset($_POST['brand_id']) || isset($_POST['color_id']) || isset($_POST['size_id'])) {
                 $filterService->getArticlesFromSearch($_POST);
             }
         }
@@ -189,10 +188,7 @@ class HomeController extends AbstractController
             ];
             $wishlistManager->insert($wish);
             header('Location:/');
-        } else {
-            $_SESSION['flash_message'] = ['Article déjà dans votre wishlist'];
-            header('Location:/');
-        }
+        } 
     }
 
     public function dislike(int $id)
